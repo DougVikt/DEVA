c29b34498cbb6fbfbeb317b8b1b6d5c4ae75f29a
DougVikt-99 <106497954+DougVikt@users.noreply.github.com>
2022-10-10 21:49:39 -0300

    Update part-calculadora.py

--- part-calculadora.py
+++ part-calculadora.py
@@ -1,14 +1,25 @@
 def calcular(n1,n2,op="!"):
     try:
 
-        if op == "+":
-            resut=n1 + n2 
-        elif op == "-":
-            resut=n1 - n2 
-        elif op== "*":
-            resut=n1 * n2 
-        elif op=="/":
-            resut=n1 / n2 
-        return print(resut)
+        if "mais" in op:
+            resultado=n1 + n2 
+        elif "menos" in op:
+            resultado=n1 - n2 
+        elif "vezes" in op:
+            resultado=n1 * n2 
+        elif "dividir" in op:
+            resultado=n1 / n2 
+        elif "potencia" in op:
+            if n2 == 0 :
+                n2 = 2
+            resultado=n1**n2
+        elif "raiz" in op:
+            resultado=n1**(1/2)
+        elif "porcentagem" in op:
+            resultado=n1*(n2/100)
+        return resultado      
     except :
         print("ERRO !!")
+
+   
+     
