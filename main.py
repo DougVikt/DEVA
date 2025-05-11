f6a234b385094b9240464efcbeb69f31dfc7791b
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2025-05-11 01:54:26 -0300

    recomençando

---
+++ main.py
@@ -0,0 +1,10 @@
+# Autor : DougVikt 
+
+# Funções :
+# um assistente virtual animada 
+# que pode executar comandos de voz
+# Reconhecimento de voz
+# Conversão de texto para fala
+# controle de software do computador
+# editar arquivos em geral 
+# salvar comandos no arquivo config
